153dbf20ab09645744d0bae1bea1eb31a4f1c3ae
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-07-29 20:25:41 -0300

    Add files via upload

---
+++ Aula02/Projeto04/src/projeto04/Projeto04.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto04;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Projeto04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int x;
+        x = 0;
+        
+        do
+        {
+            System.out.println("x vale: " + x);
+            x = x + 1;
+            
+        }while(x < 10);
+    }
+    
+}
