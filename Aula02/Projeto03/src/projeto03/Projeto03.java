28366aac6a0ebdedd0ec5f2e6c00f60218062e04
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-07-29 20:25:25 -0300

    Add files via upload

---
+++ Aula02/Projeto03/src/projeto03/Projeto03.java
@@ -0,0 +1,14 @@
+package projeto03;
+
+public class Projeto03 {
+
+    public static void main(String[] args) {
+        int x;
+        
+        for(x = 0; x < 10; x = x + 1)
+        {
+            System.out.println("x vale: " + x);
+        }
+    }
+    
+}
