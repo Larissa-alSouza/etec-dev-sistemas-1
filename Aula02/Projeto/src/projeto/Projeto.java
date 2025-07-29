b4b04f0d6e8d808bdac458bffc71ce544170bdb9
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-07-29 20:24:29 -0300

    Add files via upload

---
+++ Aula02/Projeto/src/projeto/Projeto.java
@@ -0,0 +1,24 @@
+package projeto;
+
+public class Projeto {
+
+    public static void main(String[] args) {
+        
+        int x;
+        x = 1;
+        
+        switch(x)
+        {
+            case 0:
+                System.out.println("x é 0");
+                break;
+            case 1:
+                System.out.println("x é 1");
+                break;
+            default:
+                System.out.println("x tem um valor diferente de 0 e 1");
+                break;
+        }
+    }
+    
+}
