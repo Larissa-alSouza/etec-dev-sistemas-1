493435f49832d3ea2e8b4705cd8420e30a9ba8ef
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-07-29 20:24:58 -0300

    Add files via upload

---
+++ Aula02/Projeto02/src/projeto02/Projeto02.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto02;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Projeto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x;
+        x = 0;
+        
+        while(x < 10)
+        {
+            System.out.println("x vale: " + x);
+            x = x + 1;
+        }
+    }
+    
+}
