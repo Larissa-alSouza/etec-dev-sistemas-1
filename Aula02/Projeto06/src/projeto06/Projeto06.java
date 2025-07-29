c9a3f4e2e7c6585c661813a86c87819f1b4ac848
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-07-29 20:26:56 -0300

    Add files via upload

---
+++ Aula02/Projeto06/src/projeto06/Projeto06.java
@@ -0,0 +1,17 @@
+package projeto06;
+
+public class Projeto06 {
+
+    public static void main(String[] args) {
+        
+        String texto;
+        int tamanho;
+        
+        texto = "Hello World!!!";
+        
+        tamanho = texto.length();
+        
+        System.out.println("O texto tem " + tamanho + " letras");
+    }
+    
+}
