fdf5f0af35d076db3ab3c3699829413b0b6db269
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-07-29 20:26:13 -0300

    Add files via upload

---
+++ Aula02/Projeto05/src/projeto05/Projeto05.java
@@ -0,0 +1,13 @@
+package projeto05;
+
+public class Projeto05 {
+
+    public static void main(String[] args) {
+        
+        String texto;
+        texto = "Hello World!!!";
+        
+        System.out.println(texto);
+    }
+    
+}
