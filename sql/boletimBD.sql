5bee5c95f7ae8d1283d5b04557679eb8c49ef880
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-10-28 20:39:49 -0300

    Create database and table for boletim
    Script de banco de dados do projeto boletim - Aula 08

---
+++ sql/boletimBD.sql
@@ -0,0 +1,16 @@
+create database banco;
+
+use banco;
+
+create table boletim (
+	ra int primary key,
+	materia varchar(100), 
+    b1 float, 
+    b2 float,
+    b3 float,
+    b4 float,
+    media float
+); 
+
+show databases;
+select * from boletim;
